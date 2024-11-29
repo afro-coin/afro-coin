7b6a7d6f2ca2f168f331a33b9bff89932ccee1ff
jlbrown5-ncat <60116362+jlbrown5-ncat@users.noreply.github.com>
2024-11-29 16:16:45 -0500

    Update chainparams.cpp

--- src/kernel/chainparams.cpp
+++ src/kernel/chainparams.cpp
@@ -91,13 +91,13 @@ public:
             uint256{"00000000000002dc756eebf4f49723ed8d30cc28a5f108eb94b1ba88ac4f9c22"}, SCRIPT_VERIFY_NONE);
         consensus.script_flag_exceptions.emplace( // Taproot exception
             uint256{"0000000000000000000f14c35b2d841e986ab5441de8c585d5ffe55ea1e395ad"}, SCRIPT_VERIFY_P2SH | SCRIPT_VERIFY_WITNESS);
-        consensus.BIP34Height = 227931;
-        consensus.BIP34Hash = uint256{"000000000000024b89b42a942fe0d9fea3bb44ab7bd1b19115dd6a759c0808b8"};
-        consensus.BIP65Height = 388381;          // 000000000000000004c2b624ed5d7756c508d90fd0da2c7c679febfa6c4735f0
-        consensus.BIP66Height = 363725;          // 00000000000000000379eaa19dce8c9b722d46ae6a57c2f1a988119488b50931
-        consensus.CSVHeight = 419328;            // 000000000000000004a1b34462cb8aeebd5799177f7a29cf28f2d1961716b5b5
-        consensus.SegwitHeight = 481824;         // 0000000000000000001c8018d9cb3b742ef25114f27563e3fc4a1902167f9893
-        consensus.MinBIP9WarningHeight = 483840; // segwit activation height + miner confirmation window
+        consensus.BIP34Height = 675;
+        consensus.BIP34Hash = uint256{"00000000120daf41731d93701f26a4ddf76272fe341db900fbe8ff1638e7de09"};
+        consensus.BIP65Height = 675;             // 00000000120daf41731d93701f26a4ddf76272fe341db900fbe8ff1638e7de09
+        consensus.BIP66Height = 675;             // 00000000120daf41731d93701f26a4ddf76272fe341db900fbe8ff1638e7de09
+        consensus.CSVHeight = 675;               // 00000000120daf41731d93701f26a4ddf76272fe341db900fbe8ff1638e7de09
+        consensus.SegwitHeight = 700;         // 0000000000000000001c8018d9cb3b742ef25114f27563e3fc4a1902167f9893
+        consensus.MinBIP9WarningHeight = 700; // segwit activation height + miner confirmation window
         consensus.powLimit = uint256{"00000000ffffffffffffffffffffffffffffffffffffffffffffffffffffffff"};
         consensus.nPowTargetTimespan = 14 * 24 * 60 * 60; // two weeks
         consensus.nPowTargetSpacing = 1 * 60; // 1 minute
@@ -113,9 +113,9 @@ public:
 
         // Deployment of Taproot (BIPs 340-342)
         consensus.vDeployments[Consensus::DEPLOYMENT_TAPROOT].bit = 2;
-        consensus.vDeployments[Consensus::DEPLOYMENT_TAPROOT].nStartTime = 1767243600; // January 1, 2026
-        consensus.vDeployments[Consensus::DEPLOYMENT_TAPROOT].nTimeout = 1769835600;   // January 31, 2026
-        consensus.vDeployments[Consensus::DEPLOYMENT_TAPROOT].min_activation_height = 709632; 
+        consensus.vDeployments[Consensus::DEPLOYMENT_TAPROOT].nStartTime = 1732942800; // November 30, 2024
+        consensus.vDeployments[Consensus::DEPLOYMENT_TAPROOT].nTimeout = 1735534800;   // December 30, 2024
+        consensus.vDeployments[Consensus::DEPLOYMENT_TAPROOT].min_activation_height = 800; 
 
         consensus.nMinimumChainWork = uint256{"000000000000000000000000000000000000000000000000000001af01af01af"};
         consensus.defaultAssumeValid = uint256{"00000000a94238d21a61dae00a9ef4946f130fd8a2b48536f52addb55885eac4"}; // 0
